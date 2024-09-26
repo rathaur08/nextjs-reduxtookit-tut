21682e8be9fcfeb529aaf1030058a985d81a27a2
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-09-26 15:26:17 +0530

    .#5 Component for Redux AND .#6 Add Slice
    .#6 Add Slice
    .#6 Add Slice

---
+++ src/app/components/AddUser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddUser = () => {
+  return (
+    <div>
+      <h3>AddUser</h3>
+      <input type='text' placeholder='Add' />
+      <button className='' type='button'>Add User</button>
+    </div>
+  )
+}
+
+export default AddUser
---
+++ src/app/components/DisplayUser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DisplayUser = () => {
+  return (
+    <div>
+      <h3>DisplayUser</h3>
+      {/* <input type='text' placeholder='Add' /> */}
+      {/* <button className='' type='button'>Add User</button> */}
+    </div>
+  )
+}
+
+export default DisplayUser
