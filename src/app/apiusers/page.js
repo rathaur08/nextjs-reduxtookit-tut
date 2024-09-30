89f91ddf75cbb32e90165268168008da0c1dc168
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-09-30 17:42:28 +0530

    .#15.1 API call AND Get Data Without Button Click useing [useEffect]

--- src/app/apiusers/page.js
+++ src/app/apiusers/page.js
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../page.module.css";
 import { fetchApiUsers } from "../redux/slice";
@@ -8,19 +9,24 @@ const page = () => {
   const apiUserData = useSelector((data) => data.usersData.userAPIData);
   console.log(apiUserData);
 
+  useEffect(() => {
+    dispatch(fetchApiUsers())
+  }, [])
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         {/* apiusers */}
         <h2>User list from API</h2>
-        <button onClick={() => dispatch(fetchApiUsers())}>Load Users</button>
+        <hr />
+        {/* <button onClick={() => dispatch(fetchApiUsers())}>Load Users</button> */}
         {
           apiUserData && apiUserData.map((item) => {
             return (
               <div key={item.id}>
-                <h1>
+                <p>
                   {item.name}
-                </h1>
+                </p>
               </div>
             );
           })
